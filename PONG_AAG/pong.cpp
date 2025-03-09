7eb458207273204d72ca8afb2ebc185047653ae9
aguaguirre <aguirre.agua@ciencias.unam.mx>
2025-03-08 22:20:48 -0600

    dimensiones & posición

--- PONG_AAG/pong.cpp
+++ PONG_AAG/pong.cpp
@@ -1,11 +1,23 @@
 #include <GL/glut.h>
 #include <math.h>
 
-// DIMENSIONS & AESTHETICS
+// TAMAÑO Y ESTÉTICA
 
-	//WINDOW HEIGHT WIDTH
-	//PADDLE HEIGHT WIDTH
-	//BALL HEIGHT WIDTH
+	// DIMENSIONES DE VENTANA
+const int VENTANA_WIDTH = 1000;
+const int VENTANA_HEIGHT = 800;
+	
+	// DIMENSIONES DE LAS PALETAS
+const float PALETA_WIDTH = 25.0f;
+const float PALETA_HEIGHT = 100.0f;
+
+	//POSICIÓN DE LAS PALETAS
+float izqPaletaY = 0.0f;
+float derPaletaY = 0.0f;
+
+	// DIMENSIONES DE LA PELOTA
+const float PELOTA_WIDTH = 15.0f;
+const float PELOTA_HEIGHT = 15.0f;
 
 // FUNCTIONS
 	//BALL SPEED
