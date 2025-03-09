75ddea1ce4c832c1091ef52d61f297e563677f5a
aguaguirre <aguirre.agua@ciencias.unam.mx>
2025-03-09 15:16:20 -0600

    cambio PELOTA & callbacks, reshape function

--- PONG_AAG/pong.cpp
+++ PONG_AAG/pong.cpp
@@ -17,8 +17,11 @@ float izqPaletaY = 0.0f;
 float derPaletaY = 0.0f;
 
 	// DIMENSIONES DE LA PELOTA
-const float PELOTA_WIDTH = 15.0f;
-const float PELOTA_HEIGHT = 15.0f;
+const float PELOTA_TAMANO = 15.0f;
+float pelotaX = VENTANA_WIDTH / 2.0f;
+float pelotaY = VENTANA_HEIGHT / 2.0f;
+
+
 
 	// VELOCIDAD DE LA PELOTA
 float velPelotaX = 10.0f;
@@ -32,10 +35,12 @@ int derPuntos = 0;
 void display();
 void keyboard(unsigned char, int, int);
 void update(int);
+void reshape(int, int);
 void drawPaleta(float x, float y);
 void drawPelota(float x, float y);
 void drawTexto(float x, float y, const char* text);
 
+// MAIN
 int main(int argc, char** argv) {
 
 	// INICIALIZACIÓN DE GLUT
@@ -45,19 +50,50 @@ int main(int argc, char** argv) {
 	glutCreateWindow("PONG");
 
 	// CALLBACKS 
-	//utDisplayFunc(display);
-	//utKeyboardFunc(keyboard);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutTimerFunc(0, update, 0);
+
+	// COLOR VENTANA
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	// LOOP GLUT
+	glutMainLoop();
+	return 0;
 }
 
+// DISPLAY
 void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	
+	// DIBUJAR PALETAS
 	drawPaleta(0.0f, izqPaletaY);
 	drawPaleta(VENTANA_WIDTH - PALETA_WIDTH, derPaletaY);
 
-	//drawPelota();
+	// DIBUJAR PELOTA
+	drawPelota(pelotaX, pelotaY);
+	glutSwapBuffers();
 
+	// MARCADOR DE PUNTOS
 	char marPuntos[100];
 	sprintf_s(marPuntos, "IZQ: %d  DER: %d", izqPuntos, derPuntos);
-	drawTexto(VENTANA_WIDTH / 2 - 100, VENTANA_HEIGHT - 60, marPuntos
-	);
+	drawTexto(VENTANA_WIDTH / 2 - 100, VENTANA_HEIGHT - 60, marPuntos);
+
+}
+
+// ADAPTACIÓN A LA VENTANA
+void reshape(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, VENTANA_WIDTH, 0.0, VENTANA_HEIGHT);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+// CONTROLES DEL JUEGO
+void keyboard(unsigned char key, int x, int y) {
+
 }
 
